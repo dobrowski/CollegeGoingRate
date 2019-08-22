6e7dbb9b4d9f3062e2d26f9dfa41cace739fed31
David Dobrowski <dobrowski@gmail.com>
2019-08-22 10:10:12 -0700

    Updates to labels and including graphs

--- cgr.R
+++ cgr.R
@@ -6,9 +6,18 @@ library(tidyverse)
 library(here)
 library(ggthemes)
 library(RColorBrewer)
+library(scales)
 
 
-cgr <- read_tsv( "cgr12mo18.txt", col_types = "ffccccccffffnnnnnnnnnnnn")
+cgr <- read_tsv( "cgr12mo18.txt", col_types = "ffccccccffffnnnnnnnnnnnn") %>%
+  mutate(PercentUC = `Enrolled UC (12 Months)`/`High School Completers`,
+         PercentCSU = `Enrolled CSU (12 Months)`/`High School Completers`,
+         PercentCC = `Enrolled CCC (12 Months)`/`High School Completers`,
+         PercentPrivate = `Enrolled In-State Private (2 and 4 Year) (12 Months)`/`High School Completers`,
+         PercentOutCA = `Enrolled Out-of-State (12 Months)`/`High School Completers`,
+         PercentTotal = PercentUC+PercentCSU+PercentCC+PercentPrivate+PercentOutCA,
+         name = paste0(DistrictName," - \n",SchoolName)) %>% 
+  mutate(name = fct_reorder(name, PercentTotal))
 
 
 ### College Type -----
@@ -19,15 +28,13 @@ cgr.mry <- cgr %>%
            CompleterType == "TA",
            CharterSchool == "All",
            AlternativeSchoolAccountabilityStatus == "All",
-           !is.na(`High School Completers`))  %>%
-    mutate(PercentUC = `Enrolled UC (12 Months)`/`High School Completers`,
-           PercentCSU = `Enrolled CSU (12 Months)`/`High School Completers`,
-           PercentCC = `Enrolled CCC (12 Months)`/`High School Completers`,
-           PercentPrivate = `Enrolled In-State Private (2 and 4 Year) (12 Months)`/`High School Completers`,
-           PercentOutCA = `Enrolled Out-of-State (12 Months)`/`High School Completers`,
-           PercentTotal = PercentUC+PercentCSU+PercentCC+PercentPrivate+PercentOutCA,
-           name = paste0(DistrictName," - ",SchoolName)) %>% 
-    mutate(name = fct_reorder(name, PercentTotal))
+           !is.na(`High School Completers`))  %>% 
+  mutate(DistrictName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                     TRUE ~ DistrictName)) %>%
+  mutate(SchoolName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                  TRUE ~ SchoolName)) %>%
+  mutate(name = paste0(DistrictName," - \n",SchoolName)) %>% 
+  mutate(name = fct_reorder(name, PercentTotal))
 
 ggplot(cgr.mry) +
     geom_col( aes(x = name, y = PercentTotal )) +
@@ -44,7 +51,13 @@ ggplot(cgr.stack) +
     geom_bar( aes(x =name , fill = where, weight = value )) +
     theme(axis.text.x = element_text(angle = 90)) +
     theme_hc() +
-    scale_fill_brewer(palette="Dark2")
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+    scale_fill_brewer(palette="Dark2") +
+  labs(title = "Most college attendees are at community colleges",
+       x = "",
+       y = "Percentage of Completers Attending College") + 
+   scale_fill_brewer(palette="Dark2",name = "Type of College", labels = c("Community College", "CSU", "Out of California", "Private School", "UC"))
+
 
 ggsave("All by College Type.png", width = 14, height = 10, dpi = 500)
 
@@ -57,15 +70,13 @@ cgr.gender <- cgr %>%
            CompleterType == "TA",
            CharterSchool == "All",
            AlternativeSchoolAccountabilityStatus == "All",
-           !is.na(`High School Completers`))  %>%
-    mutate(PercentUC = `Enrolled UC (12 Months)`/`High School Completers`,
-           PercentCSU = `Enrolled CSU (12 Months)`/`High School Completers`,
-           PercentCC = `Enrolled CCC (12 Months)`/`High School Completers`,
-           PercentPrivate = `Enrolled In-State Private (2 and 4 Year) (12 Months)`/`High School Completers`,
-           PercentOutCA = `Enrolled Out-of-State (12 Months)`/`High School Completers`,
-           PercentTotal = PercentUC+PercentCSU+PercentCC+PercentPrivate+PercentOutCA,
-           name = paste0(DistrictName," - ",SchoolName)) %>% 
-    mutate(name = fct_reorder(name, PercentTotal))
+           !is.na(`High School Completers`))  %>% 
+  mutate(DistrictName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                  TRUE ~ DistrictName)) %>%
+  mutate(SchoolName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                TRUE ~ SchoolName)) %>%
+  mutate(name = paste0(DistrictName," - \n",SchoolName)) %>% 
+  mutate(name = fct_reorder(name, PercentTotal)) 
 
 ggplot(cgr.gender) +
     geom_col( aes(x = name,
@@ -91,15 +102,13 @@ cgr.eth <- cgr %>%
            CompleterType == "TA",
            CharterSchool == "No",
            AlternativeSchoolAccountabilityStatus == "All",
-           !is.na(`High School Completers`))  %>%
-    mutate(PercentUC = `Enrolled UC (12 Months)`/`High School Completers`,
-           PercentCSU = `Enrolled CSU (12 Months)`/`High School Completers`,
-           PercentCC = `Enrolled CCC (12 Months)`/`High School Completers`,
-           PercentPrivate = `Enrolled In-State Private (2 and 4 Year) (12 Months)`/`High School Completers`,
-           PercentOutCA = `Enrolled Out-of-State (12 Months)`/`High School Completers`,
-           PercentTotal = PercentUC+PercentCSU+PercentCC+PercentPrivate+PercentOutCA,
-           name = paste0(DistrictName," - \n",SchoolName)) %>% 
-    mutate(name = fct_reorder(name, PercentTotal))
+           !is.na(`High School Completers`))  %>% 
+  mutate(DistrictName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                  TRUE ~ DistrictName)) %>%
+  mutate(SchoolName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                TRUE ~ SchoolName)) %>%
+  mutate(name = paste0(DistrictName," - \n",SchoolName)) %>% 
+  mutate(name = fct_reorder(name, PercentTotal)) 
 
 ggplot(cgr.eth) +
     geom_col( aes(x = name,
@@ -129,15 +138,13 @@ cgr.AG <- cgr %>%
            CompleterType %in% c("AGY","AGN"),
            CharterSchool == "All",
            AlternativeSchoolAccountabilityStatus == "All",
-           !is.na(`High School Completers`))  %>%
-    mutate(PercentUC = `Enrolled UC (12 Months)`/`High School Completers`,
-           PercentCSU = `Enrolled CSU (12 Months)`/`High School Completers`,
-           PercentCC = `Enrolled CCC (12 Months)`/`High School Completers`,
-           PercentPrivate = `Enrolled In-State Private (2 and 4 Year) (12 Months)`/`High School Completers`,
-           PercentOutCA = `Enrolled Out-of-State (12 Months)`/`High School Completers`,
-           PercentTotal = PercentUC+PercentCSU+PercentCC+PercentPrivate+PercentOutCA,
-           name = paste0(DistrictName," - ",SchoolName)) %>% 
-    mutate(name = fct_reorder(name, PercentTotal))
+           !is.na(`High School Completers`))  %>% 
+  mutate(DistrictName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                  TRUE ~ DistrictName)) %>%
+  mutate(SchoolName = case_when(AggregateLevel == "C" ~ "Monterey County",
+                                TRUE ~ SchoolName)) %>%
+  mutate(name = paste0(DistrictName," - \n",SchoolName)) %>% 
+  mutate(name = fct_reorder(name, PercentTotal)) 
 
 ggplot(cgr.AG) +
     geom_col( aes(x = name,
@@ -146,9 +153,68 @@ ggplot(cgr.AG) +
               position = position_dodge2(preserve = "single")) +
     theme(axis.text.x = element_text(angle = 90)) +
     theme_hc() +
-    labs(title = "Female students attend college at a higher rate than male students almost everywhere",
+    labs(title = "Students who met A-G attend college at a higher rate than students that did not",
          x = "",
          fill = "Gender2") + 
     scale_fill_brewer(palette="Dark2",name = "", labels = c("Met A-G", "Not Meet A-G"))
 
 ggsave("A-G.png", width = 14, height = 10, dpi = 500)
+
+
+
+### College Type by County -----
+
+cgr.all.counties <- cgr %>%
+  filter(AggregateLevel  %in% c("C"),
+         ReportingCategory == "TA",
+         CompleterType == "TA",
+         CharterSchool == "All",
+         AlternativeSchoolAccountabilityStatus == "All",
+         !is.na(`High School Completers`))  %>% 
+  mutate(name = CountyName,
+    name = fct_reorder(name, PercentTotal)) %>%
+  select(name, starts_with("Percent")) %>%
+  select(-PercentTotal) %>% 
+  gather(key = "where","value", -name)
+
+
+ggplot(cgr.all.counties) +
+  geom_bar( aes(x =name , fill = where, weight = value )) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  theme_hc() +
+  scale_fill_brewer(palette="Dark2") +
+  labs(title = "Monterey County is in the middle of Counties by College Attendance Rates",
+       x = "",
+       fill = "Gender2") + 
+  scale_fill_brewer(palette="Dark2",name = "Type of College", labels = c("Community College", "CSU", "Out of California", "Private School", "UC"))
+
+
+ggsave("County Comparison.png", width = 14, height = 10, dpi = 500)
+
+
+### College Type by Gender -----
+
+cgr.gender.type <- cgr %>%
+  filter(str_detect(CountyName, "Monterey"),
+         AggregateLevel  %in% c("S","C"),
+         ReportingCategory %in% c("GM","GF"),
+         CompleterType == "TA",
+         CharterSchool == "All",
+         AlternativeSchoolAccountabilityStatus == "All",
+         !is.na(`High School Completers`))  %>% 
+  mutate(name = fct_reorder(name, PercentTotal)) %>%
+  select(name, ReportingCategory, starts_with("Percent")) %>%
+  select(-PercentTotal) %>% 
+  gather(key = "where","value", -name, -ReportingCategory)
+
+
+ggplot(cgr.gender.type) +
+  geom_bar( aes(x =ReportingCategory , fill = where, weight = value )) +
+ # facet_grid(~name) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  theme_hc() +
+  scale_fill_brewer(palette="Dark2") +
+  labs(title = "Most college attendees are community colleges",
+       x = "",
+       fill = "Gender2") + 
+  scale_fill_brewer(palette="Dark2",name = "Type of College", labels = c("Community College", "CSU", "Out of California", "Private School", "UC"))
